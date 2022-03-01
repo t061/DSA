4b98e73f27c0989a2e66dc1f060dff0886dc2d49
Tarun Chaudhary <tarun3416@gmail.com>
2022-03-01 21:10:23 +0530

    Question

--- printSubsetofArray.java
+++ printSubsetofArray.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class printSubsetofArray {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        sub(arr, n, 0, "");
+    }
+    public static void sub(int[] arr,int n,int i,String osf) {
+        if(i==n){
+            System.out.println(osf);
+            return;
+        }
+        sub(arr,n,i+1,osf+arr[i]);
+        sub(arr,n,i+1,osf);
+    }
 }
